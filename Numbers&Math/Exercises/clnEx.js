d4188655435edb6f1a5547744e86135f5a5bd575
kylerobins <darkspidersteewy@gmail.com>
2023-11-22 13:20:29 +0300

    Challenge Exercise sltn

---
+++ Numbers&Math/Exercises/clnEx.js
@@ -0,0 +1,28 @@
+// Challenge Exercise:
+
+// Solve the following problem using JavaScript:
+
+// A car is traveling at a constant speed of `60` kilometers per hour. Write JavaScript code to calculate and log the following:
+
+// 1. The distance the car will travel in `3` hours.
+// 2. The time it will take for the car to cover a distance of `180` kilometers.
+
+
+// Solution
+let speed = 60;
+let time = 3
+// 1. The distance the car will travel in `3` hours.
+
+// Distance = Speed * Time
+
+let distance = speed * time;
+console.log("Distance Covered in 3 hours:", distance, "Km");
+
+// 2. The time it will take for the car to cover a distance of `180` kilometers.
+
+// Time = Distance/speed
+
+let distance2 = 180;
+let time2 = distance2 / speed;
+console.log("Time taken to cover 180 Km:", time2, "Hours");
+
