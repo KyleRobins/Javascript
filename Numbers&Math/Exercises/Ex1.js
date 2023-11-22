5cad57300f4a35bc8e1061c46b100e3d9c13c22a
kylerobins <darkspidersteewy@gmail.com>
2023-11-22 11:30:42 +0300

    Exercise 1 soltns

---
+++ Numbers&Math/Exercises/Ex1.js
@@ -0,0 +1,39 @@
+// ### Exercise 1: Basic Math Operations - Soltns
+
+// Write JavaScript code to perform the following operations and log the results using `console.log`:
+
+// 1. Add two numbers: `15` and `7`.
+// 2. Subtract `8` from `20`.
+// 3. Multiply `5` by `6`.
+// 4. Divide `50` by `10`.
+
+//Addition
+let num1 = 15;
+let num2 = 7;
+
+let sum = num1 + num2;
+console.log( "Addition", "" + sum);
+
+//Subtraction 
+let num3 = 8;
+let num4 = 20;
+
+let sub = num3 - num4;
+console.log("Subtraction", "" + sub);
+
+//Multiplication
+
+let num5 = 5;
+let num6 = 6;
+
+let mult = num5 * num6 ;
+console.log("Multiplication", "" + mult);
+
+//Division
+
+let num7 = 50;
+let num8 = 10;
+
+let div = num7/num8;
+console.log("Division", "" + div);
+
