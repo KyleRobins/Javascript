45a7e36b48fbbd0e9e81b5d01264735efbcdee5c
kylerobins <darkspidersteewy@gmail.com>
2023-11-22 13:09:13 +0300

    Exercise 5 soltns

---
+++ Numbers&Math/Exercises/Ex5.js
@@ -0,0 +1,27 @@
+// Exercise 5: Practical Scenario
+
+// Imagine you have a rectangular field with a length of `20` meters and a width of `15` meters. Write JavaScript code to calculate and log the following:
+
+// 1. The area of the rectangular field.
+// 2. The perimeter of the rectangular field.
+
+//Solutions
+
+let length = 20;
+let width = 15;
+
+// 1. The area of the rectangular field.
+
+// Area is calculated by multiplying length * width 
+
+let area = length * width;
+console.log("The Area of the Rectangle is:", area);
+
+
+// 2. The perimeter of the rectangular field.
+
+// Perimeter is calculated by adding the four sides of a rectangle
+
+let perimeter = length * 2 + width * 2;
+console.log("The Perimeter of the Rectangle is:", perimeter);
+
