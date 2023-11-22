10c5e690ecd8a8f05d1edd7578a43bbed45e08b9
kylerobins <darkspidersteewy@gmail.com>
2023-11-22 12:29:40 +0300

    Exercise 4 soltns

--- Numbers&Math/Exercises/Ex4.js
+++ Numbers&Math/Exercises/Ex4.js
@@ -5,12 +5,14 @@
 // 1. Calculate the result of `(10 + 5) * 3`.
 // 2. Find the average of `25`, `30`, and `35`.
 
+//solutions 
+
 // 1. Calculate the result of `(10 + 5) * 3`.
 
 let result = (10 + 5) * 3;
 console.log("The result of (10 + 5) * 3 is:", result);
 
-// Find the average of `25`, `30`, and `35`
+// 2. Find the average of `25`, `30`, and `35`
 
 let average = (25 + 30 + 35) / 3;
 console.log("The avarage of 25,30 and 35 is:", average);
