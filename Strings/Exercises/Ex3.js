d11c9c4868eaa21e9668cfead88782afd6b3a589
kylerobins <robinsmicky@gmail.com>
2023-11-24 12:53:23 +0300

    Numeric Coercion

---
+++ Strings/Exercises/Ex3.js
@@ -0,0 +1,8 @@
+// Exercise 3: Numeric Coercion
+// Create a variable age with your age as a number. 
+// Convert it to a string and concatenate it with the following message: "I am [age] years old." Log the result to the console.
+
+
+let myAge = 22;
+let message = "I am " + myAge + " years old";
+console.log(message);
