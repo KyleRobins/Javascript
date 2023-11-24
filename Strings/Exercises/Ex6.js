9bed9cefcc6ba1914bee2cc6993de91535226310
kylerobins <robinsmicky@gmail.com>
2023-11-24 13:49:01 +0300

    String Length

---
+++ Strings/Exercises/Ex6.js
@@ -0,0 +1,8 @@
+// Exercise 6: String Length
+// Find and log the length of the string "JavaScript Basics" without manually counting characters.
+
+
+let varLength = "Javascript Basics"
+
+console.log("Length of my string is:", varLength.length);
+
