984ef1504cd172598a940d09f1e5955451c22e92
kylerobins <darkspidersteewy@gmail.com>
2023-11-24 12:05:25 +0300

    String Concatenation

---
+++ Strings/Exercises/Ex1.js
@@ -0,0 +1,14 @@
+// Exercise 1: Basic Concatenation
+// Create a variable firstName with your first name and a variable lastName with your last name. 
+// Concatenate them to form a full name and store it in a variable called fullName. Log the result to the console.
+
+
+//Solutn
+
+let firstName = "Kyle";
+let lastName = " Robins";
+let fullName = firstName + lastName; 
+console.log(fullName);
+
+
+
