7bf2e2796668140a2ac647ff5cb0f5c2820c7dc5
kylerobins <darkspidersteewy@gmail.com>
2023-11-24 12:19:48 +0300

    String Interpolation

---
+++ Strings/Exercises/Ex2.js
@@ -0,0 +1,12 @@
+// Exercise 2: String Interpolation inserting values directly into the string 
+// Use string interpolation to create a message that includes the fullName from the previous exercise. 
+// The message should be in the format: "Hello, [full name]! Welcome to JavaScript basics."
+
+let firstName = "Kyle";
+let lastName = " Robins";
+let fullName = firstName+ "" +lastName;
+let welcomeMessage = `Hello, ${fullName} Welcome to javascript basics.`;
+console.log(welcomeMessage);
+
+
+
