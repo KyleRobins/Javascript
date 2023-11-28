a10e68f93b13fec57172b631776e2e3d49a2b51e
kylerobins <robinsmicky@gmail.com>
2023-11-28 09:37:43 +0300

    Js Substrings

---
+++ Strings/Exercises/Ex8.js
@@ -0,0 +1,7 @@
+// Exercise 8: Substring
+// Given the string "JavaScript," extract and log only the "Script" part without modifying the original string.
+
+let originalString = "Javascript";
+let extractedString = originalString.substring(4);
+console.log(extractedString)
+
