ee04cc210455f6556ecb71acf88277e854890b11
kylerobins <robinsmicky@gmail.com>
2023-11-28 11:04:50 +0300

    Numbers

---
+++ Numbers/Exercises/Ex1.js
@@ -0,0 +1,39 @@
+// ### Exercise 1: Basic Math Operations - Soltns
+
+// Write JavaScript code to perform the following operations and log the results using `console.log`:
+
+// 1. Add two numbers: `15` and `7`.
+// 2. Subtract `8` from `20`.
+// 3. Multiply `5` by `6`.
+// 4. Divide `50` by `10`.
+
+//Addition
+let num1 = 15;
+let num2 = 7;
+
+let sum = num1 + num2;
+console.log( "The Sum of adding 15 and 7 is:", sum);
+
+//Subtraction 
+let num3 = 8;
+let num4 = 20;
+
+let sub = num3 - num4;
+console.log("Subtracting 8 from 20 we get:", sub);
+
+//Multiplication
+
+let num5 = 5;
+let num6 = 6;
+
+let mult = num5 * num6 ;
+console.log("Multiplying 5 and 6 we get:", mult);
+
+//Division
+
+let num7 = 50;
+let num8 = 10;
+
+let div = num7/num8;
+console.log("Dividing 50 by 10 we get:", div);
+
---
+++ Numbers/Exercises/Ex2.js
@@ -0,0 +1,25 @@
+// Exercise 2: Modulus Operator - Soltns
+// Write JavaScript code to find the remainder of the following divisions using the modulus operator % and log the results:
+
+// The remainder when 25 is divided by 7.
+// The remainder when 144 is divided by 12.
+// The remainder when 17 is divided by 4.
+
+
+// 1. The remainder when 25 is divided by 7.
+
+let remainder1 = 25 % 7;
+console.log("The remainder when 25 is divided by 7 is:", remainder1);
+
+//2. The remainder when 144 is divided by 12.
+
+let remainder2 = 144 % 12;
+console.log("The remainder when 144 is divided by 12 is:", remainder2);
+
+//3. The remainder when 17 is divided by 4.
+
+let remainder3 = 17 % 4;
+console.log("The remainder when 17 is divided by 4 is:", remainder3);
+
+
+
