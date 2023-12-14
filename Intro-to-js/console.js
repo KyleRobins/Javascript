3cde0cfa7566a73d4d0d67fabea891a156af3101
kylerobins <robinsmicky@gmail.com>
2023-12-14 15:47:44 +0300

    Basics Of Js(Console)

---
+++ Intro-to-js/console.js
@@ -0,0 +1,24 @@
+// ---------- CONSOLE ----------
+console.log("Hello World");
+// log
+// clg -- vscode emmet to call console.log()
+// console.log(console); // console object
+
+console.error("This is an ERROR");
+console.warn("Warning");
+console.table({ name: "Kyle", age: 23, location: ["Kenya"] });
+
+// ---------- COMMENTS ----------
+
+// This is one line comment
+/*
+    This is 
+    multi-line 
+        comment
+*/
+/** Unordered Comment
+ * bla
+ * bla
+ * bla
+ */
+
