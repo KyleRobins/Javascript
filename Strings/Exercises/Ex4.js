b0fdfb69cb6095b24948587ea20092fdf8c69967
kylerobins <robinsmicky@gmail.com>
2023-12-15 15:56:01 +0300

    Deleted Folders

--- Strings/Exercises/Ex4.js
+++
@@ -1,7 +0,0 @@
-// Exercise 4: Escape Characters
-// Create a string that includes a newline character (\n) to print two lines of text. 
-// The first line should be "Hello," and the second line should be "World!"
-
-
-let string = "Hello\nWorld";
-console.log(string);
--- Strings/Exercises/Ex9.js
+++
@@ -1,7 +0,0 @@
-// Exercise 9: String Conversion
-// Convert a numeric value (e.g., 42) to a string and concatenate it with the string " is the answer to everything." Log the result.
-
-let num = 42
-let string = num.toString() + " is the answer to everything."
-
-console.log(string);
