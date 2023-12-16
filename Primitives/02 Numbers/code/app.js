36cfa8e5e6cab9b91c9a459dc04f2dfa34622498
kylerobins <robinsmicky@gmail.com>
2023-12-16 10:48:34 +0300

    Numbers

---
+++ Primitives/02 Numbers/code/app.js
@@ -0,0 +1,20 @@
+// JavaScript has one number type
+// Positive Number
+// Negative Number
+// Whole Number (integers)
+// Decimal Numbers
+
+const randomNum = 2024;
+const favNum = 79;
+let num = 2;
+
+//Basic Math
+
+console.log(num + num);
+console.log(num - num);
+console.log(num * num);
+console.log(num / num);
+console.log(num % num);
+console.log(num ** num);
+console.log(num++);
+console.log(num--);
