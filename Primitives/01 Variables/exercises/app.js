01729e6dd3ae18e5180d2ed9c2962e025d9715fa
kylerobins <robinsmicky@gmail.com>
2023-12-16 10:46:43 +0300

    Variables

---
+++ Primitives/01 Variables/exercises/app.js
@@ -0,0 +1,20 @@
+// 1.Create variable name (name) & store your name
+// 2.Create variable name (whatDoyouWannBecomeInYourLife) & store "Programmer"
+// 3.Create variable name (gender) & store your gender.
+// 4.Create variable name (twitterHandle) & store your twitter handle
+// 5. Finally log all your variables to the console.
+
+let name = "Kyle Robins";
+let whatDoyouWannBecomeInYourLife = "programmer";
+let gender = "M";
+let twitterHandle = "@_kylerobins";
+
+console.log(
+  "My name is " +
+    name +
+    " I am a " +
+    whatDoyouWannBecomeInYourLife +
+    " who is of gender male. " +
+    "my twitter handle is " +
+    twitterHandle
+);
