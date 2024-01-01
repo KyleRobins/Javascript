0fb023cd4be52e10b52f76ae51ed3e8fd1629e51
Kyle Robins <robinsmicky@gmail.com>
2024-01-01 21:38:45 +0300

    Javascript Arrays

---
+++ 03. Reference Types (Basics)/01. Arrays/code/app.js
@@ -0,0 +1,36 @@
+
+// -----------------------------------
+// An array is an object that can store multiple values at once. For example,
+// const words = ["hello", "world", "welcome"];
+// ['string', 123, true, []]
+// -----------------------------------
+
+// -----------------------------------
+
+// Empty Array
+
+const myList = [];
+
+// array of numbers
+
+const numbersArray = [1, 2, 3, 4, 5];
+console.log(numbersArray);
+
+
+// array of strings
+
+const stringArray = ["eat", "code", "sleep", "repeat"]
+console.log(stringArray);
+
+// accessing items from your array
+
+console.log(stringArray[0]);
+console.log(stringArray[1]);
+console.log(stringArray[2]);
+console.log(stringArray[3]);
+
+
+// Nested Array
+
+const nestArr = ['one', ['two', 'three'], 1, true, false];
+console.log(nestArr[1]);
