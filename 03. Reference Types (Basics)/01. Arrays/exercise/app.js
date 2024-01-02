7ad932389770b1226d38815ce5b88b66ab3c089d
Kyle Robins <robinsmicky@gmail.com>
2024-01-02 17:30:01 +0300

    Array Exercises

---
+++ 03. Reference Types (Basics)/01. Arrays/exercise/app.js
@@ -0,0 +1,17 @@
+// 1. Create array name (favSingers) Store 3 favorite singers.
+// 2. log the first singer in that array.
+// 3. Create array name (favNumbers) & store 4 fav Numbers.
+// 4. Create array name (mixedArr) store ["string", ["otherarray"], 123, true]
+// 5. Now Access each item in that array by using [] notation.
+
+const favSingers = ["Tate Mcrae", "Justin Beiber", "The Weekend"];
+console.log(favSingers[0]);
+
+const favNumbers = [1, 2, 3, 4, 5];
+console.log(favNumbers);
+
+const mixedArr = ["Kyle Robins", [{ task: "code" }], 366, true];
+console.log(mixedArr[0]);
+console.log(mixedArr[1][0]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
