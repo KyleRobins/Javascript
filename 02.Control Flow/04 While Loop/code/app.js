cca9bddd9215bcb926315bd5ffdcb12356dbf3ba
kylerobins <robinsmicky@gmail.com>
2025-01-13 14:36:51 +0300

    lock in

--- 02.Control Flow/04 While Loop/code/app.js
+++
@@ -1,8 +0,0 @@
-// while (condition){ code block to be executed}
-
-let i = 1;
-
-while (1 <= 5) {
-  console.log(i);
-  i++;
-}
--- 02.Control Flow/05 Do-While Loop/code/app.js
+++
@@ -1,7 +0,0 @@
-let i = 1;
-
-do {
-  console.log("Kyle is a nerd");
-  i++;
-} while (i <= 5);
-
